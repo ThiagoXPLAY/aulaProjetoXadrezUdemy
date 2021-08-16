422edee82f87369bee2458c0882e4597442c6b41
ThiagoXPLAY <trandrade31@gmail.com>
2021-08-16 01:02:40 -0300

    Teste IntelliJ no Fedora via Terminal

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -5,6 +5,7 @@ import xadrez.PartidaDeXadrez;
 
 public class Programa {
     public static void main(String[] args) {
+        
         PartidaDeXadrez partida = new PartidaDeXadrez();
         UI.imprimirTabuleiro(partida.getPecas());
     }
