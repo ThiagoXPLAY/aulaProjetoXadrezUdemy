e373f2282119d1dabbbd6433000a7225060979a1
ThiagoXPLAY <trandrade31@gmail.com>
2021-09-05 00:00:16 -0300

    Teste de commit IntelliJ

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -5,7 +5,6 @@ import xadrez.PartidaDeXadrez;
 
 public class Programa {
     public static void main(String[] args) {
-        
         PartidaDeXadrez partida = new PartidaDeXadrez();
         UI.imprimirTabuleiro(partida.getPecas());
     }
